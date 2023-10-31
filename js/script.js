490c40d75ed7e88774a932fd8d22fcbcfd80d351
Kikecode <108308253+kikenriq@users.noreply.github.com>
2023-10-30 20:43:40 -0400

    Added customs

--- js/script.js
+++ js/script.js
@@ -26,3 +26,25 @@ const changeTheme = () => {
 }
 
 $themeBtn.addEventListener("click", changeTheme)
+
+
+
+const /** {NodeList} */ $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let /*** {NodeElement} */ [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let /*** {NodeElement} */ [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function () {
+
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+
+        const /** {NodeElement} */ $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this;
+
+    });
+});
